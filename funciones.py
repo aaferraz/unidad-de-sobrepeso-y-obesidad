506fffce94c0652ab0f8ca4a2440cc0b71a4cb82
aaferraz <aaferraz.govea@gmail.com>
2025-11-21 19:26:45 -0400

    arreglado

--- funciones.py
+++ funciones.py
@@ -1,4 +1,9 @@
 from pygame import *
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QCore import Qt
+from PyQt5.QWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
 from random import *
+
+#ventana 1
+txt_title = 'Salud de Sobre Peso'
+win_x, win_y = 200, 100
+win_widht, win_height = 1000, 600
--- ventana 1.py
+++ ventana 1.py
@@ -1,4 +1,18 @@
 from funciones import *
 
-class Win_1 (QtWidgets):
-    def __init__()
+class Win_1 (QWidgets):
+    def __init__(self):
+        QWidget().__init__()
+        self.set_appear()
+        self.initUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.setWindowTitle('REGLAS')
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+    def initUI(self):
+       pass 
+    def connect(self):
+        pass
